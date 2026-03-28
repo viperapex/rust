494a11da6b37b6d47ababac5a5c9a0e09eed971e
-Drunken_Viper - <viperapex@github.com>
2026-03-28 03:15:29 +0000

    updted new note

--- rustlive1/src/main.rs
+++ rustlive1/src/main.rs
@@ -1,15 +1,16 @@
-//Ownership Rules
-//---------------------------
-//1 Each value in Rust has a variable that's its owner.
-//2 There can be only one owner at a time
-//3 When the owner goes out of scope, the value will be dropped
+// References and Borrowing
+// Safety and Performance
+// Borrowing and reference anre power concepts
+
+// Understanding References
+// References: Enable you to borrow values without taking ownership
+// Immutable Reference.
+// Mutable Reference.
 
 fn main() {
-    let s1 = String::from("RUST");
-    let len = calculate_length(&s1);
-    println!("Length of '{}' id '{}.", s1, len)
-}
+    let _x: i32 = 5;
+    let _r = &_x;
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+    println!("value of _x : {}", _x);
+    println!("value of _r : {}", _r);
 }
